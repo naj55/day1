a0759df3de8064ab1b1d57cab85d3dfabdd0a174
najla <naldossary58@gmail.com>
2023-05-01 13:30:07 +0300

    lab day2 with ejs

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
 const express = require("express");
 //step 2
 const app = express();
+let studentGrade = 96;
 
+app.get("/studentGrade", (req, res) => {
+  res.render("studentGrade.ejs", { studentGrade: studentGrade });
+});
 //step 4
 app.get("/Name", (req, res) => {
   res.send(`welcom to Najla`);
